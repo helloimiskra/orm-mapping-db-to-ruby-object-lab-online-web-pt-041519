24ec6117c1572f22db2cff913e22d1972fd49595
Iskra B <ibatistapoblete@gmail.com>
2019-07-03 07:29:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -74,6 +74,18 @@ class Student
     end
   end
 
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = "10"
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row).name
+    end.first
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
